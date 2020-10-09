e02df4d96691f0f8a8743fca1d7afb1935789ed5
Ifera <jackmtaylor.jmt@gmail.com>
2020-10-09 17:17:06 +0500

    ComplexMyPlotForm: migrate to libForms

--- src/MyPlot/forms/ComplexMyPlotForm.php
+++ src/MyPlot/forms/ComplexMyPlotForm.php
@@ -2,21 +2,27 @@
 declare(strict_types=1);
 namespace MyPlot\forms;
 
-use dktapps\pmforms\CustomForm;
+use libforms\CustomForm;
 use MyPlot\MyPlot;
 use MyPlot\Plot;
 use pocketmine\player\Player;
 
 abstract class ComplexMyPlotForm extends CustomForm implements MyPlotForm {
+
 	/** @var Plot|null $plot */
 	protected $plot;
 
-	public function __construct(string $title, array $elements, \Closure $onSubmit) {
-		parent::__construct($title, $elements, $onSubmit,
-			function(Player $player) : void {
+	public function __construct(?Player $player, string $title, array $elements, ?\Closure $onSubmit = null) {
+		parent::__construct($player, $onSubmit ?? static function(Player $player): void {
 				$player->getServer()->dispatchCommand($player, MyPlot::getInstance()->getLanguage()->get("command.name"), true);
 			}
 		);
+
+		$this->setTitle($title);
+
+		foreach($elements as $element){
+            $this->addElement($element);
+        }
 	}
 
 	/**
